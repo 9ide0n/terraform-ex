45d6e6f1adf3cd1b679e7c4370ac32629377485d
9ide0n <denis.fomichev2017@yandex.ru>
2018-02-01 18:00:05 +0300

    1) Add security group to allow external access on port 8080 2) Link security group with instance with terraform interpolation (seq group creates earlier as seen in terraform graph so we can use value from it)

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-41e0b93b"
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
     user_data = <<-EOF
             #!/bin/bash
             echo "Hello, World" > index.html
@@ -13,5 +14,16 @@ resource "aws_instance" "example" {
 
     tags {
         Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
