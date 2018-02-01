c5a3604a33a185139e93fd2d98d943c763704673
9ide0n <denis.fomichev2017@yandex.ru>
2018-02-01 17:19:45 +0300

    1) Emulate web server with running user_data script after instance creation 2) Add tag

--- main.tf
+++ main.tf
@@ -5,4 +5,13 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-41e0b93b"
     instance_type = "t2.micro"
+    user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, World" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+
+    tags {
+        Name = "terraform-example"
+    }
 }
