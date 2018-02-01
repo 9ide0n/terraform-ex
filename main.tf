0c3a70ea0dc4809bd6208ac3fd61d180a0010be1
9ide0n <denis.fomichev2017@yandex.ru>
2018-02-01 18:25:09 +0300

    1) Add input variable to allow DRY principe with default 8080 (may not specify here - enter by hands to console running plan/apply, pass as -var server_port="8080" param or export TF_VAR_derver_port from bash) 2) Add output variable for easy search of public_ip like so: curl `terraform output public_ip`:8080

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
     region = "us-east-1"
 }
 
+variable "server_port"{
+    description = "Port for HTTP requests" 
+    default = 8080
+}
+
+output "public_ip"{
+    value = "${aws_instance.example.public_ip}"
+}
+
 resource "aws_instance" "example" {
     ami = "ami-41e0b93b"
     instance_type = "t2.micro"
@@ -9,7 +18,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
             #!/bin/bash
             echo "Hello, World" > index.html
-            nohup busybox httpd -f -p 8080 &
+            nohup busybox httpd -f -p "${var.server_port}" &
             EOF
 
     tags {
@@ -21,8 +30,8 @@ resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = "${var.server_port}"
+        to_port = "${var.server_port}"
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
