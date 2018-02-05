2b9a5aec4ab8f544d53030cd15f24b2ad00ffd35
9ide0n <denis.fomichev2017@yandex.ru>
2018-02-05 16:58:43 +0300

    Configure cluster of webservers (defined by aws_launch_configuration) with ASG and ELB with automatic recreation of the failed nodes based on ELB healthchecks
    test with curl://http:<elb_dns_name> - remove running instance, watch process of automatic  recreation of instance, elb work in AWS web-interface

--- asg/main.tf
+++ asg/main.tf
@@ -7,27 +7,51 @@ variable "server_port"{
     default = 8080
 }
 
-output "public_ip"{
-    value = "${aws_instance.example.public_ip}"
+
+output "elb_dns_name"{
+    value = "${aws_elb.example.dns_name}"   
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-41e0b93b"
+# instead of instance we use launch_configuration for asg with same params as instance
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-41e0b93b"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+    security_groups = ["${aws_security_group.instance.id}"]
     user_data = <<-EOF
             #!/bin/bash
             echo "Hello, World" > index.html
             nohup busybox httpd -f -p "${var.server_port}" &
             EOF
+    # we must sure that instance created by launch_configuration will be successfully created, only then the old-one will be destroyed
+    # this attribute should be set to all linked primitives in our case aws_security_group
+    lifecycle {
+        create_before_destroy = true
+    }
+}
 
-    tags {
-        Name = "terraform-example"
+# fetch aws_availability_zones datasource from AWS API 
+data "aws_availability_zones" "all" {}
+
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = "${aws_launch_configuration.example.id}"
+    # allow asg to work with all AZs we get from AWS with data_source call
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    # when new instance creating/destroying register it into ELB
+    load_balancers = ["${aws_elb.example.name}"]
+    # use ELB healthchecks to auto replace instances if they are dead/unhealthy
+    health_check_type = "ELB"
+    min_size = 2
+    max_size = 10
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
     }
 }
 
 resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
+    name = "terraform-aws-launch-configuration-instance"
 
     ingress {
         from_port = "${var.server_port}"
@@ -35,4 +59,52 @@ resource "aws_security_group" "instance" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
 }
+
+resource "aws_elb" "example" {
+    name = "terraform-asg-example"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb.id}"]
+
+    # configure elb to route traffic to instanses port
+    listener {
+        lb_port = 80
+        lb_protocol = "http"
+        instance_port = "${var.server_port}"
+        instance_protocol = "http"
+    }
+
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        interval = 30
+        # check health by sending HTTP GET request to / uri
+        target = "HTTP:${var.server_port}/"
+    }
+
+}
+
+resource "aws_security_group" "elb" {
+    name = "terraform-example-elb"
+    # allow incoming http requests to elb
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    # allow outgoing traffic to allow elb to make healthchecks of balanced instances
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+} 
