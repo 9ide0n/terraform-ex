935877aba3ac04b4d7750534dad83eea99e7849c
9ide0n <denis.fomichev2017@yandex.ru>
2018-02-06 16:15:24 +0300

    Add prod env with separate base and webserver-cluster link to its base and based on the module + asg schedule

---
+++ prod/data-stores/mysql/outputs.tf
@@ -0,0 +1,6 @@
+output "address" {
+    value = "${aws_db_instance.prod.address}"
+}
+output "port" {
+    value = "${aws_db_instance.prod.port}"
+}
---
+++ prod/services/webserver-cluster/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "webserver_cluster" {
+    source = "../../../../terraform-modules/services/webserver-cluster/"
+    cluster_name = "webservers-prod"
+    db_remote_state_bucket = "9ide0n-s3-terraform-state"
+    db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
+    instance_type = "t2.micro"
+    min_size = 2
+    max_size = 10
+}
+
+# add two asg shedules based on asg name created at module
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+    scheduled_action_name = "scale-out-during-business-hours"
+    min_size = 2
+    max_size = 10
+    desired_capacity = 5
+    recurrence = "0 9 * * *"
+    autoscaling_group_name = "${module.webserver_cluster.asg_name}"
+}
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+    scheduled_action_name = "scale-in-at-night"
+    min_size = 2
+    max_size = 10
+    desired_capacity = 2
+    recurrence = "0 17 * * *"
+    autoscaling_group_name = "${module.webserver_cluster.asg_name}"
+}
