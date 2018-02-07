bde04722004eae9eca342480b80e1357de3cadd2
9ide0n <denis.fomichev2017@yandex.ru>
2018-02-07 21:57:39 +0300

    Move ASG scheduling to module (API v 0.1.0) give us API with enable_autoscaling=true/false to turn it on/off respectively

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -3,29 +3,13 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-    source = "git::git@github.com:9ide0n/terraform-modules.git//services/webserver-cluster?ref=v0.0.1"
+    source = "git::git@github.com:9ide0n/terraform-modules.git//services/webserver-cluster?ref=v0.1.0"
     cluster_name = "webservers-prod"
     db_remote_state_bucket = "9ide0n-s3-terraform-state"
     db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
     instance_type = "t2.micro"
     min_size = 2
     max_size = 10
+    enable_autoscaling=true
 }
 
-# add two asg shedules based on asg name created at module
-resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
-    scheduled_action_name = "scale-out-during-business-hours"
-    min_size = 2
-    max_size = 10
-    desired_capacity = 5
-    recurrence = "0 9 * * *"
-    autoscaling_group_name = "${module.webserver_cluster.asg_name}"
-}
-resource "aws_autoscaling_schedule" "scale_in_at_night" {
-    scheduled_action_name = "scale-in-at-night"
-    min_size = 2
-    max_size = 10
-    desired_capacity = 2
-    recurrence = "0 17 * * *"
-    autoscaling_group_name = "${module.webserver_cluster.asg_name}"
-}
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,13 +3,15 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-    source = "git::git@github.com:9ide0n/terraform-modules.git//services/webserver-cluster?ref=v0.0.2"
+    # source = "../../../../terraform-modules/services/webserver-cluster"
+    source = "git::git@github.com:9ide0n/terraform-modules.git//services/webserver-cluster?ref=v0.1.0"
     cluster_name = "webservers-stage"
     db_remote_state_bucket = "9ide0n-s3-terraform-state"
     db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
     instance_type = "t2.micro"
     min_size = 2
     max_size = 2
+    enable_autoscaling=false
 }
 # add open port to security group defined in module for testing
 resource "aws_security_group_rule" "allow_testing_inbound" {
